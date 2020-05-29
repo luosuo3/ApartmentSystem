5dc22a114a63ba8cb2e0683819d3f93e1d6bc23c
wangzheng <718652232@qq.com>
2020-05-30 00:48:33 +0800

    修改改为跳转修改,客房管理按钮改为启用和禁用状态.

--- src/main/java/com/apartment/management/controller/admin/RoomManageController.java
+++ src/main/java/com/apartment/management/controller/admin/RoomManageController.java
@@ -10,20 +10,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Controller
 public class RoomManageController {
+    private int roomId;
     @Autowired
     RoomManageMapper roomManageMapper;
+
     @GetMapping("/roomManage")
     public String roomManage(Model model) {
         RoomManageExample roomManageExample = new RoomManageExample();
         roomManageExample.createCriteria().andIdIsNotNull();
         List<RoomManage> roomManages = roomManageMapper.selectByExample(roomManageExample);
-        model.addAttribute("roomManages",roomManages);
+        model.addAttribute("roomManages", roomManages);
         return "/admin/room_management";
     }
 
@@ -32,7 +38,7 @@ public class RoomManageController {
         String id = request.getParameter("id");
         Integer id1 = Integer.valueOf(id);
         RoomManage roomManage = roomManageMapper.selectByPrimaryKey(id1);
-        if (roomManage.getStatus()==0) {
+        if (roomManage.getStatus() == 0) {
             roomManage.setStatus(1);
         } else {
             roomManage.setStatus(0);
@@ -41,7 +47,39 @@ public class RoomManageController {
         RoomManageExample roomManageExample = new RoomManageExample();
         roomManageExample.createCriteria().andIdIsNotNull();
         List<RoomManage> roomManages = roomManageMapper.selectByExample(roomManageExample);
-        model.addAttribute("roomManages",roomManages);
+        model.addAttribute("roomManages", roomManages);
+        return "/admin/room_management";
+    }
+
+    @PostMapping("/getManageId")
+    public String getManageId(HttpServletRequest request, Model model) {
+        String id = request.getParameter("id");
+        roomId = Integer.parseInt(id);
+        RoomManage roomManage = roomManageMapper.selectByPrimaryKey(Integer.valueOf(id));
+        model.addAttribute("roomManage", roomManage);
+        return "admin/room_modefy";
+    }
+
+    @PostMapping("/modefyRoom")
+    public String modefyRoom(HttpServletRequest request,Model model, @RequestParam(name = "roomNums" )int roomNums, @RequestParam(name = "username")String username, @RequestParam(name = "inTime")String inTime,@RequestParam(name = "outTime")String outTime) throws ParseException {
+        String checkIn = request.getParameter("checkIn");
+        String status = request.getParameter("status");
+        RoomManage roomManage = new RoomManage();
+        roomManage.setId(roomId);
+        roomManage.setRoomNums(roomNums);
+        roomManage.setUsername(username);
+        roomManage.setCheckIn(Integer.valueOf(checkIn));
+        roomManage.setStatus(Integer.valueOf(status));
+        SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd");
+        Date inTime1 = sd.parse(inTime);
+        roomManage.setInTime(inTime1);
+        Date outTime1=sd.parse(outTime);
+        roomManage.setOutTime(outTime1);
+        roomManageMapper.updateByPrimaryKey(roomManage);
+        RoomManageExample roomManageExample = new RoomManageExample();
+        roomManageExample.createCriteria().andIdIsNotNull();
+        List<RoomManage> roomManages = roomManageMapper.selectByExample(roomManageExample);
+        model.addAttribute("roomManages", roomManages);
         return "/admin/room_management";
     }
 }
