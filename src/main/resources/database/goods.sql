e127f262320c5fb328f892992d8b9886e9500bcd
SerenceJ <2142349037@qq.com>
2020-06-03 08:39:20 +0800

    商品管理弹出添加页面

--- src/main/resources/database/goods.sql
+++ src/main/resources/database/goods.sql
@@ -1,12 +1,11 @@
-create table good_manage
-(
-    goods_id     int auto_increment comment '商品ID'
-        primary key,
-    goods_nums   int         not null comment '商品编号
-',
-    goods_name   varchar(56) not null comment '商品名称',
-    goods_price  float       not null comment '商品单价',
-    goods_number int         null comment '商品数量'
-);
+
+CREATE TABLE `good_manage` (
+  `goods_id` int NOT NULL AUTO_INCREMENT COMMENT '商品ID',
+  `goods_nums` int NOT NULL COMMENT '商品编号\n',
+  `goods_name` varchar(56) NOT NULL COMMENT '商品名称',
+  `goods_price` float DEFAULT NULL COMMENT '商品单价',
+  `goods_number` int DEFAULT NULL COMMENT '商品数量',
+  PRIMARY KEY (`goods_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 
